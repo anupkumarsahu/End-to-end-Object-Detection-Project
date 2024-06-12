a54d96261fc1d8fdf1a5d122b33cde53d6f4234e
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2024-06-12 19:39:23 +0530

    Data Ingestion

---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+ARTIFACTS_DIR: str = "artifacts"
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_DOWNLOAD_URL: str = "https://github.com/anupkumarsahu/datasets/raw/main/Sign_language_data.zip"
--- signLanguage/logger/__init__.py
+++ signLanguage/logger/__init__.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from from_root import from_root
 
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(from_root(), 'log', LOG_FILE)
