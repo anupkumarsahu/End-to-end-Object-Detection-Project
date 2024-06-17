98da9979c104759f9940d65bec8758d63312e550
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2024-06-17 09:21:44 +0530

    Training model

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# detection > 2024-06-05 6:13pm
+https://universe.roboflow.com/aiml-ewqb8/detection-c8jjo
+
+Provided by a Roboflow user
+License: MIT
+
--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -5,3 +5,15 @@ ARTIFACTS_DIR: str = "artifacts"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_DOWNLOAD_URL: str = "https://github.com/anupkumarsahu/datasets/raw/main/Sign_language_data.zip"
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_STATUS_FILE:str = 'status.txt'
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+MODEL_TRAINER_NO_EPOCHS: int = 1
+MODEL_TRAINER_BATCH_SIZE: int = 16
+
+BUCKET_NAME = "sign-lang-2024"
+S3_MODEL_NAME = "best.pt"
